6e537253d25058ab8997a0853e07ab670b4ba436
Blerand Bahtiri <id12bbi@cs.umu.se>
2016-12-06 15:56:55 +0100

    add styled alert

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,13 +1,28 @@
 console.log('\'Allo \'Allo!');
 
 var player, iframe;
-var $ = document.querySelector.bind(document);
+
 $(document).ready(function(){
+	
+	
+    $("#success-alert").hide();
+    $("#myWish").click(function showAlert() {
+        $("#success-alert").alert();
+        $("#success-alert").fadeTo(2000, 500).slideUp(500, function(){
+        	$("#success-alert").slideUp(500);
+        });
+    });
+ 	
+/*
 
    	$( "button" ).click(function() {
  		$( "player" ).hide( "slow" );
  		alertFunc();
  	}
+
+ 	$( "p" ).click(function(){
+        alertFunc();
+    });*/
 	  
 	// init player
 
@@ -45,32 +60,16 @@ $(document).ready(function(){
 	function alertFunc(){
 		alert('Hello');
 
-		// Get the modal
-
-	var modal = document.getElementById('myModal');
-
 	// Get the button that opens the modal
 	var btn = document.getElementById("playerbutton");
 
 	// Get the <span> element that closes the modal
 	var span = document.getElementsByClassName("close")[0];
-
-	// When the user clicks the button, open the modal 
-	btn.onclick = function() {
-	    modal.style.display = "block";
 	}
 
-	// When the user clicks on <span> (x), close the modal
-	span.onclick = function() {
-	    modal.style.display = "none";
-	}
+	//JavaScript Document
+});
 
-	// When the user clicks anywhere outside of the modal, close it
-	window.onclick = function(event) {
-	    if (event.target == modal) {
-	        modal.style.display = "none";
-	    }
-	}// JavaScript Document
+        
 
-}
 
